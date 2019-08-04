1da9a55b8ab9b1aaecc66c1c7b2fda5a6a01b492
Xu Zhang <xu-zhang@sjtu.edu.cn>
2019-08-04 17:11:46 +0800

    SequentialSearch symbol tables
    Implements usage of Scanner

---
+++ 3-Searching/FrequencyCounter.java
@@ -0,0 +1,24 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class FrequencyCounter {
+    public static void main(String[] args) throws FileNotFoundException {
+        int minLen = 3;
+        ST<String, Integer> st = new ST<String, Integer>();
+
+        Scanner in = new Scanner(new FileReader("tale.txt"));
+        while (in.hasNext()) {
+            String word = in.next();
+            if (word.length() < minLen) continue;
+            if (!st.contains(word)) st.put(word, 1);
+            else st.put(word, st.get(word)+1);
+        }
+        String max = " ";
+        st.put(max, 0);
+        for (String word : st.keys())
+            if (st.get(word) > st.get(max)) max = word;
+
+        System.out.println(max + " " + st.get(max));
+    }
+}
---
+++ 3-Searching/ST.java
@@ -0,0 +1,41 @@
+public class ST<Key extends Comparable<Key>, Value> {
+    private Node first;
+    private class Node {
+        Key key;
+        Value value;
+        Node next;
+        public Node(Key k, Value v, Node n) {
+            this.key = k;
+            this.value = v;
+            this.next = n;
+        }
+    }
+
+    public ST() { }
+
+    public void put(Key k, Value v) {
+        for (Node i=first; i!=null; i=i.next)
+            if (k.equals(i.key)) { i.value = v; return; }
+
+        Node oldFirst = first;
+        first = new Node(k, v, oldFirst);
+    }
+
+    public Value get(Key k) {
+        for (Node i=first; i!=null; i=i.next)
+            if (k.equals(i.key)) return i.value;
+        return null;
+    }
+
+    public boolean contains(Key k) {
+        return get(k) != null;
+    }
+
+    public Iterable<Key> keys() {
+        ListQueue<Key> queue = new ListQueue<Key>();
+        for (Node i=first; i!=null; i=i.next) {
+            queue.enqueue(i.key);
+        }
+        return queue;
+    }
+}
